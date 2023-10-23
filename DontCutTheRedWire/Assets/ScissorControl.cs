651d8f61acf588c70803c5ac155cc8c81a1116ae
Artowl <52302174+Artowl@users.noreply.github.com>
2023-10-23 14:50:07 -0500

    basic models in
    scissors now "cut" wire

---
+++ DontCutTheRedWire/Assets/ScissorControl.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HandTools
+{
+    public class ScissorControl : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _scissors;
+        [SerializeField] private Collider[] _scissorColliders;
+
+        bool canCut;
+        // Start is called before the first frame update
+        void Start()
+        {
+            ActivateCutColliders(false);
+            canCut = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 closedPosition = _scissors[0].transform.forward - _scissors[1].transform.forward;
+            float scissorAngle = Vector3.Angle(closedPosition, _scissors[1].transform.forward);
+
+            Debug.Log("angle" + scissorAngle);
+            if ((scissorAngle < 95f) && canCut == false)
+            {
+                
+                ActivateCutColliders(true);
+                Debug.Log("Cut object");
+                canCut = true;
+            }
+            if ((scissorAngle < 100f) && canCut == true)
+            {
+               // ActivateCutColliders(false);
+                canCut = false;
+            }
+        }
+
+
+        private void ActivateCutColliders(bool state)
+        {
+            Debug.Log("activateColiders");
+            foreach (Collider c in _scissorColliders)
+            {
+                c.enabled = state;
+            }
+        }
+    }
+}
---
+++ DontCutTheRedWire/Assets/Scripts/ArmedOrSafe.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BombParts
+{
+    public class ArmedOrSafe : MonoBehaviour
+    {
+
+        public bool isArmed;
+        // Start is called before the first frame update
+        private void Start()
+        {
+            isArmed = true;
+            ;
+        }
+
+        private void WireCut(string aliveOrDead)
+        {
+            switch (aliveOrDead)
+            {
+                case "alive":
+                    Debug.Log("alive");
+                    break;
+                case "dead":
+                    Debug.Log("dead");
+                    break;
+            }
+            Destroy(this.gameObject);
+        }
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Scissors"))
+            {
+                if (isArmed)
+                {
+
+                    WireCut("dead");
+                }
+                if (isArmed)
+                {
+
+                    WireCut("alive");
+                }
+            }
+        }
+
+
+    }
+}
